edaa32670a4d6e8a91d652b528c06c47f1ae58b9
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2019-11-18 15:39:37 +0300

    +el_forest

---
+++ Forest2/Assets/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnGUI()
+    {
+        float posX = cam.pixelWidth / 2;
+        float posY = cam.pixelHeight / 2;
+        GUI.Label(new Rect(posX, posY, 12, 12), "*");
+    }
+}
